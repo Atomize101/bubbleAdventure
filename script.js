4edea98cd8076684b6ddf222cae027cc9effd7fb
Chris Patrick <atomize101@gmail.com>
2020-11-22 17:18:17 -0500

    Setting up Mouse interactivity

---
+++ script.js
@@ -0,0 +1,26 @@
+// Canvas setup
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 500;
+
+let score = 0;
+let gameFrame = 0;
+ctx.font = '50px Georgia';
+
+// Mouse Interactivity
+const mouse = {
+	x: canvas.width / 2,
+	y: canvas.height / 2,
+	click: false,
+};
+
+canvas.addEventListener('mousedown', (event) => {
+	mouse.x = event.x;
+	mouse.y = event.y;
+	console.log(event);
+});
+
+// Player
+// Bubbles
+// Animation loop
