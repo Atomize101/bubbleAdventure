6893d82f609e7dc83ad23a9e455ecf32f5fa9321
Chris Patrick <atomize101@gmail.com>
2020-11-24 13:39:58 -0500

    Bubble now moves when player clicks

--- script.js
+++ script.js
@@ -19,16 +19,20 @@ const mouse = {
 };
 
 canvas.addEventListener('mousedown', (event) => {
+	mouse.click = true;
 	mouse.x = event.x - canvasPosition.left;
 	mouse.y = event.y - canvasPosition.top;
-	console.log(mouse.x, mouse.y);
+});
+
+canvas.addEventListener('mouseup', (event) => {
+	mouse.click = false;
 });
 
 // Player
 
 class Player {
 	constructor() {
-		this.x = canvas.width / 2;
+		this.x = canvas.width;
 		this.y = canvas.height / 2;
 		this.radius = 50;
 		this.angle = 0;
@@ -41,14 +45,39 @@ class Player {
 	update() {
 		const dx = this.x - mouse.x;
 		const dy = this.y - mouse.y;
-		if (mouse.yx != this.x) {
+		if (mouse.x != this.x) {
 			this.x -= dx / 30;
 		}
 		if (mouse.y != this.y) {
 			this.y -= dy / 30;
 		}
 	}
+	draw() {
+		if (mouse.click) {
+			ctx.lineWidth = 0.2;
+			ctx.beginPath();
+			ctx.moveTo(this.x, this.y);
+			ctx.lineTo(mouse.x, mouse.y);
+			ctx.stroke();
+		}
+		ctx.fillstyle = 'red';
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+		ctx.fill();
+		ctx.closePath();
+		ctx.fillRect(this.x, this.y, this.radius, 10);
+	}
 }
+
+const player = new Player();
 
 // Bubbles
 // Animation loop
+
+function animate() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	player.update();
+	player.draw();
+	requestAnimationFrame(animate);
+}
+animate();
