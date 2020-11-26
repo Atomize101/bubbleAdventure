9f8f12fd61a0871429bcde94d9c7dfa3abf9a295
Chris Patrick <atomize101@gmail.com>
2020-11-26 08:01:51 -0500

    Creating the Bubbles class

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ class Player {
 			ctx.lineTo(mouse.x, mouse.y);
 			ctx.stroke();
 		}
-		ctx.fillstyle = 'red';
+		ctx.fillStyle = 'red';
 		ctx.beginPath();
 		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
 		ctx.fill();
@@ -72,12 +72,41 @@ class Player {
 const player = new Player();
 
 // Bubbles
+const bubblesArray = [];
+class Bubble {
+	constructor() {
+		this.x = Math.random() * canvas.width;
+		this.y = Math.random() * canvas.height;
+		this.radius = 50;
+		this.distance;
+	}
+	update() {
+		this.y -= this.speed;
+	}
+	draw() {
+		ctx.fillStyle = 'blue';
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.stroke();
+	}
+}
+function handleBubbles() {
+	if (gameFrame % 50 == 0) {
+		bubblesArray.push(new Bubble());
+		console.log(bubblesArray.length);
+	}
+}
+
 // Animation loop
 
 function animate() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	handleBubbles();
 	player.update();
 	player.draw();
+	gameFrame++;
 	requestAnimationFrame(animate);
 }
 animate();
